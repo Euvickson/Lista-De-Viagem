b01ce1e93e65be91b8e43bcfc9fc787c95aaa78f
Victor <victor.eugennio@gmail.com>
2022-12-22 10:29:43 -0300

    Ao colocar um nome de item que já existe, o HTML é atualizado
    TODO: Atualizar o localstorage para também ser atualizado

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ const form = document.getElementById('novoItem');
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-console.log(itens);
-
 itens.forEach( (elemento) => {
     criaElemento(elemento)
 })
@@ -12,15 +10,22 @@ form.addEventListener("submit", (evento) => {
     evento.preventDefault();
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
+
+    const existe = itens.find( elemento => elemento.nome === nome.value);
     
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     };
-    
-    criaElemento(itemAtual);
-    
-    itens.push(itemAtual)
+
+    if (existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+    } else {
+        itemAtual.id = itens.length;
+        criaElemento(itemAtual);
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens));
     
@@ -35,9 +40,14 @@ function criaElemento (item) {
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
+}
+
+function atualizaElemento (item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
